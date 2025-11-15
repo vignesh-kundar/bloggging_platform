83e615580e72a7e5d52566fb2bed2bee390d4207
vignesh <vigneshkundar125@gmail.com>
2025-11-16 02:15:02 +0530

    init spring blog site

---
+++ src/main/java/com/vignesh/spring_blog/Controller/Health.java
@@ -0,0 +1,14 @@
+package com.vignesh.spring_blog.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Health {
+
+    @GetMapping("/ping")
+    String health() {
+        return "Pong!!. Application is health.";
+    }
+
+}
