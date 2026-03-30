26f6135180099d01291b7c471cdc7adbf79902de
vignesh <vigneshkundar125@gmail.com>
2026-03-31 00:24:00 +0530

    Use Properties for Expiration & secret Key

--- src/main/java/com/vignesh/spring_blog/service/JwtService.java
+++ src/main/java/com/vignesh/spring_blog/service/JwtService.java
@@ -2,7 +2,9 @@ package com.vignesh.spring_blog.service;
 
 import com.vignesh.spring_blog.repository.UserAuthProviderRepository;
 import com.vignesh.spring_blog.repository.UsersRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
@@ -10,30 +12,37 @@ import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Slf4j
 @Service
 public class JwtService {
 
-    @Autowired
-    private UsersRepository usersRepository;
+    @Value("${app.jwt.secret}")
+    protected String JwtSecretKey;
 
-    @Autowired
-    private UserAuthProviderRepository userAuthProviderRepository;
+    @Value("${app.jwt.expiration}")
+    protected String JwtExpiration;
 
     public String generateJwtToken( String email ) {
         SecretKey key = Jwts.SIG.HS256.key().build();
         // need to update expiry time :)
-        String jwt = Jwts.builder().subject(email).signWith(key).expiration(new Date(96000)).compact();
+        String jwt = Jwts.builder().subject(email).signWith(key).expiration(new Date(System.currentTimeMillis() + JwtExpiration)).compact();
         return jwt;
     }
 
-    public String extractEmail( String jwt ) {
+    public String extractEmail( String jwt ) throws Exception {
         SecretKey key = Jwts.SIG.HS256.key().build();
         String email = Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt).getPayload().getSubject();
         return email;
     }
 
-    public boolean isTokenValid() {
-       // need to code here
-        return false;
+    public boolean isTokenValid( String jwt ) {
+       try {
+           String subject = extractEmail(jwt);
+           log.info("Extracted jwt token for : {}" , subject);
+           return true;
+       } catch (Exception ex) {
+           log.error("Failed to extract Jwt Token");
+           return false;
+       }
     }
 }
