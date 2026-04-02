b8a746cbd5157f97ac8e186f75c77c98c78ffd77
vignesh <vigneshkundar125@gmail.com>
2026-04-03 00:57:46 +0530

    add jwtAuthFilter in SecurityConfiguration

--- src/main/java/com/vignesh/spring_blog/configuration/SecurityConfiguration.java
+++ src/main/java/com/vignesh/spring_blog/configuration/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.vignesh.spring_blog.configuration;
 
+import com.vignesh.spring_blog.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,10 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,7 +31,8 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET , "/api/posts").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter , UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
