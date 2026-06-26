37f97256cfdd877205fa40639fb44bc664139efd
vignesh <vigneshkundar125@gmail.com>
2026-06-26 13:45:52 +0530

    update securityConfiguration

--- src/main/java/com/vignesh/spring_blog/configuration/SecurityConfiguration.java
+++ src/main/java/com/vignesh/spring_blog/configuration/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.OPTIONS , "/**").permitAll()
                         .requestMatchers(HttpMethod.GET , "/actuator/health").permitAll()
                         .requestMatchers(HttpMethod.GET , "/ping").permitAll()
-                        .requestMatchers(HttpMethod.GET , "/api/v1/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET , "/api/v2/posts").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
